417879a2c73d77431d78ed058af82635e209bee4
Mohammad Norouzi <mnorouzidev@gmail.com>
2022-02-02 23:18:00 +0330

    Change some texts

--- src/layouts/main/index.js
+++ src/layouts/main/index.js
@@ -6,7 +6,6 @@ import { Box, Link, Container, Typography, Stack } from '@mui/material';
 // components
 import Logo from '../../components/Logo';
 //
-import MainFooter from './MainFooter';
 import MainHeader from './MainHeader';
 
 // ----------------------------------------------------------------------
@@ -18,8 +17,6 @@ MainLayout.propTypes = {
 export default function MainLayout({ children }) {
   const { pathname } = useRouter();
 
-  const isHome = pathname === '/';
-
   return (
     <Stack sx={{ minHeight: 1 }}>
       <MainHeader />
@@ -28,28 +25,23 @@ export default function MainLayout({ children }) {
 
       <Box sx={{ flexGrow: 1 }} />
 
-      {!isHome ? (
-        <MainFooter />
-      ) : (
-        <Box
-          sx={{
-            py: 5,
-            textAlign: 'center',
-            position: 'relative',
-            bgcolor: 'background.default',
-          }}
-        >
-          <Container>
-            <Logo sx={{ mb: 1, mx: 'auto' }} />
+      <Box
+        sx={{
+          py: 5,
+          textAlign: 'center',
+          position: 'relative',
+          bgcolor: 'background.default',
+        }}
+      >
+        <Container>
+          <Logo sx={{ mb: 1, mx: 'auto' }} />
 
-            <Typography variant="caption" component="p">
-              © All rights reserved
-              <br /> made by &nbsp;
-              <Link href="https://minimals.cc/">minimals.cc</Link>
-            </Typography>
-          </Container>
-        </Box>
-      )}
+          <Typography variant="caption" component="p">
+            © All rights reserved
+            <br /> made by &nbsp; Mohammad Norouzi
+          </Typography>
+        </Container>
+      </Box>
     </Stack>
   );
 }
